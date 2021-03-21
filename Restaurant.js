e1482f8ebad6dc147312447e82d921f756914185
Caleb Uhouse <calebmuhouse@gmail.com>
2021-03-21 19:53:27 -0400

    Update Restaurant.js

--- Restaurant.js
+++ Restaurant.js
@@ -1,1 +1,26 @@
+function GetMenu( MenuFileName, OutputID )
+{
+    var xmlhttp;
+
+    if ( window.XMLHttpRequest )
+    {
+        xmlhttp = new XMLHttpRequest;
+    }
+    else
+    {
+        xmlhttp = new ActiveXObject( "Microsoft.XMLHTTP" );
+    }
+    xmlhttp.onreadystatechange = function ()
+    {
+        if (( xmlhttp.readyState == 4 ) &&
+            ( xmlhttp.status == 200 ))
+        {
+            document.getElementById( OutputID ).innerHTML = xmlhttp.responseText;
+        }
+    }
+
+    xmlhttp.open( "GET", MenuFileName , true );
+
+    xmlhttp.send();
+}
 
