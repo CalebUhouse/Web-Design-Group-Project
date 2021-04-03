7067a44ee3650a2e953e30f3c26a2e7d0fecb352
Caleb Uhouse <calebmuhouse@gmail.com>
2021-04-03 08:57:33 -0400

    Create Map.php

---
+++ Map.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Map Page</title>
+    <link href="Restaurant.css" type="text/css" rel="Stylesheet">
+</head>
+<body>
+
+    <main>
+
+        <header>
+            <h1 class="section-header">
+                Redrock Tavern
+            </h1> 
+        </header>
+
+        <section>
+            <p class="section-header top-header">Where to Find Us</p>
+
+            <div id="wrapper-9cd199b9cc5410cd3b1ad21cab2e54d3">
+		    <div id="map-9cd199b9cc5410cd3b1ad21cab2e54d3"></div><script>(function () {
+            var setting = {"height":538,"width":803,"zoom":16,"queryString":"Chicken Dinner Road, Caldwell, ID, USA","place_id":"EiZDaGlja2VuIERpbm5lciBSb2FkLCBDYWxkd2VsbCwgSUQsIFVTQSIuKiwKFAoSCXMQYlrbyq9UEToRp8Bov4tmEhQKEgnLFxp4u7KvVBGcTXVDi0Lcbg","satellite":false,"centerCoord":[43.576596470648624,-116.77323904990216],"cid":"0x668bbf68c0a7113a","lang":"en","cityUrl":"/us/id/nampa-6126","cityAnchorText":"Map of Nampa, Idaho, United States","id":"map-9cd199b9cc5410cd3b1ad21cab2e54d3","embed_id":"402939"};
+            var d = document;
+            var s = d.createElement('script');
+            s.src = 'https://1map.com/js/script-for-user.js?embed_id=402939';
+            s.async = true;
+            s.onload = function (e) {
+            window.OneMap.initMap(setting)
+            };
+            var to = d.getElementsByTagName('script')[0];
+            to.parentNode.insertBefore(s, to);
+            })();</script><a href="https://1map.com/map-embed">1 Map</a></div>
+
+        </section>
+
+        <aside class="section-header">
+            <h2>
+                Promotional deals!
+            </h2>
+            <p>
+                2 Drinks for the price of 2 Drinks!
+            </p>
+        </aside>
+
+        <nav>
+            <div class="navigation-bar">
+                <a title="Home" href="Index.php">Home</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="About" href="About.php">About</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Menu" href="Menu.php">Menu</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Map Page" href="Map.php">Find Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Contact Us.php">Contact Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Takeout.php">Order Takeout</a>
+            </div>
+        </nav>
+
+        <footer>
+             <p class="section-header">
+                ~ Hours of Operation ~
+            </p>
+            <p class="section-header">
+                Monday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Tuesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Wednesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Thursday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Friday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Saturday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Sunday: Closed
+            </p>
+        </footer>
+
+    </main>
+
+</body>
+</html>
