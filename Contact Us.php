612e68245f6034193360a2e295d3024808cf5bde
Caleb Uhouse <calebmuhouse@gmail.com>
2021-04-03 08:57:23 -0400

    Create Contact Us.php

---
+++ Contact Us.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Page</title>
+    <link href="Restaurant.css" type="text/css" rel="Stylesheet">
+</head>
+<body>
+<main>
+    <header class="section-header">
+                <h1>
+                    Redrock Tavern
+                </h1>
+    </header>
+
+    <section>
+        <p class="section-header top-header">
+                Contact us!  
+        </p>
+            <p class="section-header">
+                Chicken Dinner Road, Caldwell, ID, USA
+            </p>
+            <p class="section-header">
+                Phone: 202-555-0186
+            </p>
+            <p class="section-header">
+                No Checks accepted, sorry.
+            </p>
+          </section>    
+
+        <aside class="section-header">
+            <h2>
+                Promotional deals!
+            </h2>
+            <p>
+                2 Drinks for the price of 2 Drinks!
+            </p>
+        </aside>
+
+        <nav>
+            <div class="navigation-bar">
+                <a title="Home" href="Index.php">Home</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="About" href="About.php">About</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Menu" href="Menu.php">Menu</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Map Page" href="Map.php">Find Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Contact Us.php">Contact Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Takeout.php">Order Takeout</a>
+            </div>
+        </nav>
+
+        <footer>
+            <p class="section-header">
+                ~ Hours of Operation ~
+            </p>
+            <p class="section-header">
+                Monday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Tuesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Wednesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Thursday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Friday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Saturday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Sunday: Closed
+            </p>
+        </footer>
+</main>
+</body>
+</html>
