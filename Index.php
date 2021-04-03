1825c2c31e20255ceea32b0f555cf2b82e543b82
Caleb Uhouse <calebmuhouse@gmail.com>
2021-04-03 08:57:27 -0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link href="Restaurant.css" type="text/css" rel="Stylesheet">
+</head>
+<body>
+    <main>
+        <header class="section-header">
+                <h1>
+                    Redrock Tavern
+                </h1>
+        </header>
+
+        <section>
+            <p>
+                Welcome to the Redrock Tavern! We've got the best Chicken Dinner on Chicken Dinner Road.
+                Check out our extensive menu of rustic, homestyle favorites like "chicken" and "sweet tea".
+                Dine in and enjoy the live music every other Thursday, or order Takeout. Everyone is welcome except Kevin,
+                he knows what he did.
+            </p>
+            <div>
+                 <img src="https://www.redrockcanyonlv.org/wp-content/uploads/red-rock-canyon-vista.jpg">
+            </div>
+        </section>  
+        
+
+        <aside class="section-header">
+            <h2>
+                Promotional deals!
+            </h2>
+            <p>
+                2 Drinks for the price of 2 Drinks!
+            </p>
+        </aside>
+
+        <nav>
+            <div class="navigation-bar">
+                <a title="Home" href="Index.php">Home</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="About" href="About.php">About</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Menu" href="Menu.php">Menu</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Map Page" href="Map.php">Find Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Contact Us.php">Contact Us</a>
+            </div>
+            <div class="navigation-bar">
+                <a title="Contact Page" href="Takeout.php">Order Takeout</a>
+            </div>
+        </nav>
+
+        <footer>
+         <p class="section-header">
+                ~ Hours of Operation ~
+            </p>
+            <p class="section-header">
+                Monday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Tuesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Wednesday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Thursday: 11am - 8:30pm
+            </p>
+            <p class="section-header">
+                Friday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Saturday: 11am - 9pm
+            </p>
+            <p class="section-header">
+                Sunday: Closed
+            </p>
+      </footer>
+    </main>
+</body>
+</html>
