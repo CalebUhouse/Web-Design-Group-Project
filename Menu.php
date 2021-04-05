5cf4a1c2346f68be605747843ecc639c7cf71fde
djc7941 <79331441+djc7941@users.noreply.github.com>
2021-04-04 21:43:29 -0400

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -73,26 +73,10 @@
 <?php
   PageSideBar();
 ?>
-        <nav>
-            <div class="navigation-bar">
-                <a title="Home" href="Index.php">Home</a>
-            </div>
-            <div class="navigation-bar">
-                <a title="About" href="About.php">About</a>
-            </div>
-            <div class="navigation-bar">
-                <a title="Menu" href="Menu.php">Menu</a>
-            </div>
-            <div class="navigation-bar">
-                <a title="Map Page" href="Map.php">Find Us</a>
-            </div>
-            <div class="navigation-bar">
-                <a title="Contact Page" href="Contact Us.php">Contact Us</a>
-            </div>
-            <div class="navigation-bar">
-                <a title="Contact Page" href="Takeout.php">Order Takeout</a>
-            </div>
-        </nav>
+
+<?php
+    PageNav();
+?>
 
 <?php
   PageFooter();
