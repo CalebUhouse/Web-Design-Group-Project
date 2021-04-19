e790441dab5c4b227d7ad5ada4f443d947e320fc
djc7941 <79331441+djc7941@users.noreply.github.com>
2021-04-18 23:34:53 -0400

    Update Menu.sql

--- Menu.sql
+++ Menu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 18, 2021 at 01:39 PM
+-- Generation Time: Apr 18, 2021 at 11:32 PM
 -- Server version: 10.3.25-MariaDB-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -34,6 +34,27 @@ CREATE TABLE `menu_items` (
   `description` varchar(100) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu_items`
+--
+
+INSERT INTO `menu_items` (`id`, `category`, `description`, `price`) VALUES
+('item15', 'Kids Menu', 'Chicken Tenders', '4.00'),
+('item16', 'Kids Menu', 'Hamburger', '4.00'),
+('item17', 'Kids Menu', 'Hot Dog', '4.00'),
+('item18', 'Kids Menu', 'Grilled Cheese Sandwich', '3.50'),
+('item19', 'Kids Menu', 'Pizza - pepperoni, cheese, or sausage', '4.25'),
+('item25', 'Drinks', 'Water', '0.00'),
+('item26', 'Drinks', 'Lemonade', '1.00'),
+('item27', 'Drinks', 'Raspberry Lemonade', '1.00'),
+('item28', 'Drinks', 'Coke', '1.00'),
+('item29', 'Drinks', 'Dr. Pepper', '1.00'),
+('item30', 'Drinks', 'Root Beer', '1.00'),
+('item31', 'Drinks', 'Ginger Ale', '1.00'),
+('item32', 'Drinks', 'Coffee', '1.00'),
+('item33', 'Drinks', 'Sweet Tea', '1.00'),
+('item34', 'Drinks', 'Unsweetened Tea', '1.00');
 
 --
 -- Indexes for dumped tables
