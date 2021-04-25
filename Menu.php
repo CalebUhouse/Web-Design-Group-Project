82641fa6c2bb1d6756286c09c6659af36d946d6b
CalebUhouse <79284244+CalebUhouse@users.noreply.github.com>
2021-04-25 12:09:55 -0400

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -13,57 +13,41 @@
             <div class="menu">
                 <ul>
                     <p class="menu-category">Appetizers</p> 
-                    <li>Bread Roll Basket - Comes with 4 buttered bread rolls</li>
-                    <li>Family Bread Roll Basket - Comes with 6 buttered bread rolls</li>
-                    <li>Garlic Breadsticks</li>
-                    <li>Chicken Wings - one dozen wings: plain, mild, hot, BBQ w/ bleu cheese or ranch</li>
+                    <?php
+                        Menu( "Appetizers", "Menu");
+                    ?>
                 </ul>
                 <ul>
                     <p class="menu-category">Soups and Salads</p> 
-                    <li>Grilled Chicken Caesar Salad - Romaine mix topped w/ caesar dressing, croutons, onions, and mozzarella cheese</li>
-                    <li>House Salad</li>
-                    <li>Greek Salad - mixed spring greens topped w/ kalamata olives, cucumber, pickled red onions, and feta, w/ greek dressing</li>
-                    <li>French Onion Soup</li>
-                    <li>New England Clam Chowder Soup</li>
+                    <?php
+                        Menu( "SnS", "Menu");
+                    ?>
                 </ul>
                 <ul>
                     <p class="menu-category">Main Entrees</p> 
-                    <li>Signature Chicken Dinner</li>
-                    <li>Crispy Chicken</li>
-                    <li>Grilled Chicken</li>
-                    <li>Ranch Tavern Double</li>
-                    <li>Baked Mac & Cheese - served with creamy cheddar cheese, crispy bacon bits, and bread crumbs</li>
+                    <?php
+                        Menu( "Entrees", "Menu");
+                    ?>
                 </ul>
                 <ul>
                     <p class="menu-category">Desserts</p> 
-                    <li>Ice Cream Bowl - bowl served with 3 scoops of ice cream of your choice: vanilla, chocolate, strawberry</li>
-                    <li>Carrot Cake - topped w/ cream cheese icing</li>
-                    <li>Chocolate Cake w/ Peanut Butter Icing</li>
-                    <li>Brownie Sundae - brownie served w/ vanilla ice cream and topped w/ chocalate syrup and whipped cream</li>
-                    <li>Cheesecake</li>
+                    <?php
+                        Menu( "Desserts", "Menu");
+                    ?>
                 </ul>
                 <ul>
                     <p class="menu-category">Drinks</p> 
-                    <li>Water</li>
-                    <li>Lemonade</li>
-                    <li>Raspberry Lemonade</li>
-                    <li>Coke</li>
-                    <li>Dr. Pepper</li>
-                    <li>Root Beer</li>
-                    <li>Ginger Ale</li>
-                    <li>Coffee</li>
-                    <li>Sweet Tea</li>
-                    <li>Unsweetened Tea</li>
+                    <?php
+                        Menu( "Drinks", "Menu");
+                    ?>
                 </ul>
                 <p class="section-header">* Each meal comes with a choice of two sides - mashed potatoes, french fries, macaroni & cheese, soup, or side salad</p>
                 <div class="kids-menu">
                     <p class="section-header top-header">Kid's Meals</p>
                     <ul class="menu">
-                        <li>Chicken Tenders</li>
-                        <li>Hamburger</li>
-                        <li>Hot Dog</li>
-                        <li>Grilled Cheese Sandwich</li>
-                        <li>Pizza - pepperoni, cheese, or sausage</li>
+                        <?php
+                          Menu( "Kids Menu", "Menu");
+                        ?>
                     </ul>
                     <p class="section-header">* Each meal comes with a choice of two sides - mashed potatoes, french fries, macaroni & cheese, soup, or side salad</p>
                 </div>
