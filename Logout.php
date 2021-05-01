6da054d66bd1703246dee914ffb77b7ab40ee65e
CalebUhouse <79284244+CalebUhouse@users.noreply.github.com>
2021-05-01 19:52:31 -0400

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,1 +1,10 @@
+<?php
+    // require the include
+    require "Restaurant.inc";
+    
+    // log out
+    session_destroy();
 
+    // go back to home page
+    header( "Location: Index.php" );
+?>
