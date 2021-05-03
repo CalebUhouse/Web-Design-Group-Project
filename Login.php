3dfb54d0806a8f99dc30d0ce67b4781167cf2324
djc7941 <79331441+djc7941@users.noreply.github.com>
2021-05-02 21:04:26 -0400

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,1 +1,22 @@
+<?php
 
+  require "Restaurant.inc";
+
+  HTMLStart( "Login Page" );
+
+  HTMLHeader();
+
+    PageNav();
+?>
+      
+      <section>
+        <p class="section-header top-header">WIP</p>
+        
+      </section>
+<?php
+  PageSideBar();
+
+  PageFooter();
+
+  HTMLEnd();
+?>
