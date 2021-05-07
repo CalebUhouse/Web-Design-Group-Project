a715d6c3162146071d0a056ba21dc6748ade3e11
CalebUhouse <79284244+CalebUhouse@users.noreply.github.com>
2021-05-07 15:00:32 -0400

    Update Login.php

--- Login.php
+++ Login.php
@@ -11,12 +11,14 @@
       
       <section>
         <p class="section-header top-header">WIP</p>
-        form action="login.php" method="POST">
+        <form action="login.php" method="POST">
       <p>Email Address: <input name="email" required="true"/></p>
-        <p>Password: <nput name="password" type="password" required="true" /></p>
+        <p>Password: <input name="password" type="password" required="true" /></p>
         <p><button type="submit">Log on</button></p>
       </section>
-    <
+</form>
+
+
 <?php
   PageSideBar();
 
