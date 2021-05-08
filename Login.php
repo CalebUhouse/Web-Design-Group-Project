dd96a0ab92295da51b3a7759495ee75764e55483
CalebUhouse <79284244+CalebUhouse@users.noreply.github.com>
2021-05-07 21:45:58 -0400

    Update Login.php

--- Login.php
+++ Login.php
@@ -18,10 +18,9 @@
 ?>
       
       <section>
-        <p class="section-header top-header">WIP</p>
-        <form action="Index.php" method="POST">
-      <p>Email: <input name="Email" type="email" required="true"/></p>
-        <p>Password: <input name="Password" type="password" required="true" /></p>
+        <form action= "Login.php" method="POST">
+        <p>Email Address: <input name="email" required="true"/></p>
+        <p>Password: <input name="password" type="password" required="true" /></p>
         <p><button type="submit">Log on</button></p>
         </form>
       </section>
